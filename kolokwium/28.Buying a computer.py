263394f5dccf0ebd0e967db24b058363f087bc5c
Illia-desktop <anukovich2@gmail.com>
2024-01-09 13:10:58 +0100

    'Max sum in list 3' (additional)

--- kolokwium/28.Buying a computer.py
+++ kolokwium/28.Buying a computer.py
@@ -11,12 +11,27 @@ for _ in range(n):
     models[t[0]] = dict
 
 m = int(input())
-persons = {}
+persons = []
 for i in range(m):
     dict = {}
     t = list(input().split())
     for el in t:
         dict[el.split(':')[0]] = el.split(':')[1]
-    persons[i] = dict
+    persons.append(dict)
 print(models)
 print(persons)
+
+
+for person in persons:
+    l = []
+    for key_p, value_p in person.items():
+        name = =
+        for k, dict in models.items():
+            num = 0
+            for key, value in dict.items():
+                if key == key_p:
+                    n_num = abs(int(value_p) - int(value))
+                    num = min(num, n_num)
+
+
+
---
+++ kolokwium/5.Diagonal sum.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+matrix = [list(map(int, input().split())) for _ in range(n)]
+
+diag_sum = {}
+# for i in range(n):
+#     summ = 0
+#     for j in range(n-i):
+#         summ += matrix[i+j][j]
+#     diag_sum.append(summ)
+if n == m:
+    pass
+if n < m:
+    for i in range(n):
+        for j in range(n)
+if n > m:
+    pass
+
+print(diag_sum)
