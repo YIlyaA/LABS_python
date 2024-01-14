717dade13041883b252128060a76647c3c015889
Illia-desktop <anukovich2@gmail.com>
2024-01-14 15:18:24 +0100

    'Similar strings' brute force (additional)

--- additional/Similar strings.py
+++ additional/Similar strings.py
@@ -3,21 +3,36 @@ s2 = input()
 
 
 def similar(s1, s2):
-    error = 0
     if s1 == s2:
         return "TAK"
-    if len(s1) - 1 != len(s2):
+    if abs(len(s1) - len(s2)) >= 2:
         return "NIE"
-    for i in range(len(s2)):
-        if s1[i] != s2[i]:
-            s1_new = s1[:i] + s1[i + 1:]
-            error += 1
-            if s1_new == s2:
-                return "TAK"
-    if error > 1:
-        return "NIE"
+
+    if len(s2) < len(s1):
+        ns1 = s1[:-1]
+        if ns1 == s2:
+            return "TAK"
+    elif len(s2) > len(s1):
+        ns2 = s2[:-1]
+        if ns2 == s1:
+            return "TAK"
+
+    if len(s2) < len(s1):
+        for i in range(len(s2)):
+            if s1[i] != s2[i]:
+                new_s1 = s1[0:i] + s1[i + 1:]
+                if new_s1 == s2:
+                    return "TAK"
+                else:
+                    return "NIE"
     else:
-        return "TAK"
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                new_s2 = s2[0:i] + s2[i + 1:]
+                if new_s2 == s2:
+                    return "TAK"
+                else:
+                    return "NIE"
 
 
 print(similar(s1, s2))
